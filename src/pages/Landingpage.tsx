fd02227429d25f8df04257364e811241efaa38ab
ADEDANCE <uthmanadedance@gmail.com>
2026-08-11 14:01:06 +0100

    Feat:Create pages folder

---
+++ src/pages/Landingpage.tsx
@@ -0,0 +1,15 @@
+import Features from "../Section/Features";
+import Hero from "../Section/Hero";
+import Problem from "../Section/Problem";
+
+function Landingpage() {
+  return (
+    <div>
+      <Hero />
+      <Problem />
+      <Features  />
+    </div>
+  );
+}
+
+export default Landingpage;
