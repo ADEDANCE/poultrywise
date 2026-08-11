44c6caccd2df0c098c943f26f6473a24d4701240
ADEDANCE <uthmanadedance@gmail.com>
2026-08-11 14:02:20 +0100

    Install REact icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import Navbar from "./components/Navbar";
+import Landingpage from "./pages/Landingpage";
+
 const App = () => {
-  return <div className=" text-red-600">App</div>;
+  return (
+    <div className=" text-red-600">
+      <Navbar />
+      <Landingpage />
+    </div>
+  );
 };
 
 export default App;
