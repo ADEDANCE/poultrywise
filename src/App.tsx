68a04b9b0374bb2847450743e4a5b51167acfa83
ADEDANCE <uthmanadedance@gmail.com>
2026-08-11 15:53:11 +0100

    Feat: Import Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar";
 import Landingpage from "./pages/Landingpage";
+import Footer from "./Section/Footer";
 
 const App = () => {
   return (
     <div className=" text-red-600">
       <Navbar />
       <Landingpage />
+      <Footer />
     </div>
   );
 };
