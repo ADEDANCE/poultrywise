b5a005feac4985908ac5455f7aa5884acc7afffc
ADEDANCE <uthmanadedance@gmail.com>
2026-08-12 18:17:13 +0100

    Import new pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Navbar from "./components/Navbar";
 import Landingpage from "./pages/Landingpage";
 import Footer from "./Section/Footer";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import { SetupFlock } from "./pages/SetupFlock";
+import Dashboard from "./pages/Dashboard";
 
 const App = () => {
   return (
-    <div className=" text-red-600">
+    <BrowserRouter>
       <Navbar />
-      <Landingpage />
+      <Routes>
+        <Route path="/" element={<Landingpage />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/setupflock" element={<SetupFlock />} />
+
+         <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+     
       <Footer />
-    </div>
+    </BrowserRouter>
   );
 };
 
