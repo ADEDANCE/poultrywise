aad5ee4eb4b25a1f326314fa841e53956e786058
ADEDANCE <uthmanadedance@gmail.com>
2026-08-13 17:43:08 +0100

    Import new Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,37 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Navbar from "./components/Navbar";
 import Landingpage from "./pages/Landingpage";
-import Footer from "./Section/Footer";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 import { SetupFlock } from "./pages/SetupFlock";
 import Dashboard from "./pages/Dashboard";
+import DailyRecord from "./pages/DailyRecord";
+import History from "./pages/History";
+import DashboardLayout from "./Layout/DashboardLayout";
+import PublicLayout from "./Layout/PublicLayout";
+import Setting from "./pages/Setting";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Navbar />
       <Routes>
-        <Route path="/" element={<Landingpage />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/setupflock" element={<SetupFlock />} />
+        {/* Public pages */}
+        <Route element={<PublicLayout />}>
+          <Route path="/" element={<Landingpage />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/setupflock" element={<SetupFlock />} />
+        </Route>
 
-         <Route path="/dashboard" element={<Dashboard />} />
+        {/* Dashboard layout */}
+        <Route element={<DashboardLayout />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+
+          <Route path="/dailyrecord" element={<DailyRecord />} />
+          <Route path="/history" element={<History />} />
+          <Route path="/setting" element={<Setting />} />
+        </Route>
       </Routes>
-     
-      <Footer />
     </BrowserRouter>
   );
 };
