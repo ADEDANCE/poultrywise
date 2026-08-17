a43a3544e1a34e9593ae1eab6af20a50f764c87b
ADEDANCE <uthmanadedance@gmail.com>
2026-08-17 14:38:00 +0100

    export register and login respons

---
+++ src/services/authService.ts
@@ -0,0 +1,29 @@
+import api from "./api";
+import type {
+  RegisterData,
+  RegisterResponse,
+  LoginData,
+  LoginResponse,
+} from "../types/auth";
+
+export const registerUser = async (
+  data: RegisterData
+): Promise<RegisterResponse> => {
+  const response = await api.post<RegisterResponse>(
+    "/api/auth/register",
+    data
+  );
+
+  return response.data;
+};
+
+export const loginUser = async (
+  data: LoginData
+): Promise<LoginResponse> => {
+  const response = await api.post<LoginResponse>(
+    "/api/auth/login",
+    data
+  );
+
+  return response.data;
+};
